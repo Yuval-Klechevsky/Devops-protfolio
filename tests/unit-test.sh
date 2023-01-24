939621ec31e1748a23fc2ca95cc07b997f9a8cb5
Administrator <admin@example.com>
2023-01-24 11:59:22 +0000

    checking full e2e

--- tests/unit-test.sh
+++ tests/unit-test.sh
@@ -51,6 +51,25 @@ else
   echo "PUT Request to /subscription failed." >> response.txt
 fi
 
+response=$(curl -s -o /dev/null -w "%{http_code}" -X GET -H "Content-Type: application/x-www-form-urlencoded" -d '{"first_name": "Yuval ","last_name": "Klechevsky" ,"birth_date":"27/10/2000","weight": "73","height": "174","training_program": "Full-Body"}' $API_BASE_URL/subscription/$id/BMI)
+RESPONSES+=("$response")
+if [[ $response == *"200"* ]]; then
+  echo "GET Request to /subscription/id/BMI was successful." >> response.txt
+else
+  echo "GET Request to /subscription/id/BMI failed." >> response.txt
+fi
+
+response=$(curl -s -o /dev/null -w "%{http_code}" -X DELETE -H "Content-Type: application/x-www-form-urlencoded"  $API_BASE_URL/subscription/$id)
+RESPONSES+=("$response")
+if [[ $response == *"200"* ]]; then
+  echo "DELETE Request to /subscription was successful." >> response.txt
+else
+  echo "DELETE Request to /subscription failed." >> response.txt
+fi
+
+
+
+
 for item in "${RESPONSES[@]}"; do
   if [[ $item == 200 ]]; then
     continue
