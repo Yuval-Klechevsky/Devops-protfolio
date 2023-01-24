7a9a31ca2edbad6962d5992fec26329166c2eac6
Administrator <admin@example.com>
2023-01-24 12:08:41 +0000

    checking full e2e2

--- tests/unit-test.sh
+++ tests/unit-test.sh
@@ -3,7 +3,7 @@ API_BASE_URL="3.9.146.148"
 touch response.txt
 response=$(curl -s -o /dev/null -w "%{http_code}" $API_BASE_URL/)
 RESPONSES+=("$response")
-id=$(curl -s -o /dev/null -w "%{http_code}" -X GET -H "Content-Type: application/x-www-form-urlencoded"  $API_BASE_URL/id-for-test)
+id=$(curl -s $API_BASE_URL/id-for-test)
 
 if [[ $response == *"200"* ]]; then
   echo "GET Request to / was successful." >> response.txt
