0955b0d7f0a0db933ecbac97c8d50a3f45e0311d
Administrator <admin@example.com>
2023-01-25 16:05:46 +0000

    check dockerss3 version/1.0

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 export New_tag
 docker rmi $(docker images -aq)
 docker pull 644435390668.dkr.ecr.eu-west-2.amazonaws.com/yuval-klechevsky-repo:${New_tag}
-docker compose -f docker compose-prod  build --no-cache
-docker compose -f docker compose-prod up -d
+docker compose -f docker-compose-prod  build --no-cache
+docker compose -f docker-compose-prod up -d
 
 sleep 10
 curl 3.8.133.86:80    
