6840750e83603f38f973924c4fd324970e000e13
Administrator <admin@example.com>
2023-01-25 16:07:42 +0000

    check dockerss4 version/1.0

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 export New_tag
 docker rmi $(docker images -aq)
 docker pull 644435390668.dkr.ecr.eu-west-2.amazonaws.com/yuval-klechevsky-repo:${New_tag}
+cd GYM-protfolio
 docker compose -f docker-compose-prod  build --no-cache
 docker compose -f docker-compose-prod up -d
 
