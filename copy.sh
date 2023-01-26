661466657ec133ac3d615ad170295f83257dc192
Administrator <admin@example.com>
2023-01-26 09:04:51 +0000

    checking prod2  version/7.0

--- copy.sh
+++ copy.sh
@@ -7,9 +7,9 @@ scp ./nginx.dockerfile  ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio
 scp ./docker-compose-prod.yaml  ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio
 scp ./init-db.js ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio
 scp ./app.py ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio
-scp ./tests/unit-test.sh ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio/tests
-scp ./nginx/nginx.conf ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio/nginx
-scp ./templates/index.html ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio/templates
+scp ./tests/unit-test.sh ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio/tests/
+scp ./nginx/nginx.conf ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio/nginx/
+scp ./templates/index.html ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio/templates/
 scp ./deploy.sh ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio
 ssh ubuntu@35.178.35.133 ./GYM-protfolio/deploy.sh $New_tag
 
