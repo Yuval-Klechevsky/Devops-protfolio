e556e0ba017dcb3740e7a84940a6a28936d2c2d5
Administrator <admin@example.com>
2023-01-26 09:46:55 +0000

    checking versions version/8.0

--- copy.sh
+++ copy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 New_tag=$1
 ssh ubuntu@35.178.35.133 mkdir -p GYM-protfolio
-ssh ubuntu@35.178.35.133 mkdir -p GYM-protfolio/tests
 ssh ubuntu@35.178.35.133 mkdir -p GYM-protfolio/nginx
 ssh ubuntu@35.178.35.133 mkdir -p GYM-protfolio/templates
 scp ./requirements.txt  ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio
@@ -10,7 +9,6 @@ scp ./nginx.dockerfile  ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio
 scp ./docker-compose-prod.yaml  ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio
 scp ./init-db.js ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio
 scp ./app.py ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio
-scp ./tests/unit-test.sh ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio/tests
 scp ./nginx/nginx.conf ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio/nginx
 scp ./templates/index.html ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio/templates
 scp ./deploy.sh ubuntu@35.178.35.133:/home/ubuntu/GYM-protfolio
