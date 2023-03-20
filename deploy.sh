3306bc517e7063aaec7672acf6380cb31cc4ae80
Yuval-Klechevsky <yuval.klechevsky27@gamil.com>
2023-03-20 17:14:27 +0000

    updateds version 1.3

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 New_tag=$1
 export New_tag
 docker rmi $(docker images -aq)
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 863254239753.dkr.ecr.eu-west-2.amazonaws.com
 docker pull 863254239753.dkr.ecr.eu-west-2.amazonaws.com/yuval-klechevsky-repo:${New_tag}
 cd GYM-protfolio
 docker compose -f docker-compose-prod.yaml  build --no-cache
