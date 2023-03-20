5b022d5fe59c77c46b19ec02c25f23e426259c5d
Yuval-Klechevsky <yuval.klechevsky27@gamil.com>
2023-03-20 17:04:45 +0000

    updateds version 1.0

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 New_tag=$1
 export New_tag
 docker rmi $(docker images -aq)
-docker pull 644435390668.dkr.ecr.eu-west-2.amazonaws.com/yuval-klechevsky-repo:${New_tag}
+docker pull 863254239753.dkr.ecr.eu-west-2.amazonaws.com/yuval-klechevsky-repo:${New_tag}
 cd GYM-protfolio
 docker compose -f docker-compose-prod.yaml  build --no-cache
 docker compose -f docker-compose-prod.yaml up -d
